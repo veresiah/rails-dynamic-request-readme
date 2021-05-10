3a27f5ca0be9501f97e7629069fc747fafa125bc
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-05-10 15:04:56 -0400

    commit tree be5700b75af2c126b70702f46c8cf231537da98a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  #get 'posts/:id' => 'posts#show'
+  resources :posts, only: :show 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
